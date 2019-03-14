139bb0dce87f8889e988589a4e25f430d0721602
Leonardo Fuso Nuzzo <leonardofusonuzzo@gmail.com>
2019-03-14 09:03:04 -0300

    dev - Implementação parcial de busca em profundidade

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 
 /* Estrutura TNo */
 typedef struct _TNo {
-  int vertice;
-  struct _TNo *proximo;
+    int vertice;
+    struct _TNo *proximo;
 } TNo;
 
 /* Estrutura TGrafo */
 typedef struct {
-  int V;
-  int A;
-  TNo **adj;
+    int V;
+    int A;
+    TNo **adj;
 } TGrafo;
 
 /* Declaração de funções */
@@ -23,6 +23,9 @@ void insertA (TGrafo *G, int v, int w);
 void show (TGrafo *G);
 int isPath (TGrafo *G, const int *seq, int k);
 int indeg (TGrafo *G, int w);
+void busca (TGrafo *G);
+void busca_profundidade (TGrafo *G, TNo *, int visitado[]);
+void freeGraph (TGrafo *);
 
 /* Cliente */
 int main ()
@@ -35,7 +38,7 @@ int main ()
   insertA (grafo, 0, 2);
   insertA (grafo, 0, 3);
   insertA (grafo, 0, 4);
-  insertA (grafo, 1, 2);
+  insertA (grafo, 2, 1);
   insertA (grafo, 2, 4);
   insertA (grafo, 3, 4);
   insertA (grafo, 3, 5);
@@ -47,8 +50,11 @@ int main ()
   printf ("\nisPath = %d", isPath (grafo, seq, 4));
 
   int vertice = 3;
-  int result = indeg(grafo, vertice);
-  printf("\nGrau de entrada [ %d ] -> %d ", vertice, result);
+  int result = indeg (grafo, vertice);
+  printf ("\nGrau de entrada [ %d ] -> %d ", vertice, result);
+
+  printf ("\nBusca ->");
+  busca (grafo);
 
   return 0;
 }
@@ -89,18 +95,18 @@ void insertA (TGrafo *G, int v, int w)
 
   /* Se existe aux e aux != NULL */
   while (aux && aux->vertice <= w)
-    {
+  {
 
-      /*
-       * Se o grafo já tem a aresta
-       * [ v - w ], a função não faz nada.
-       */
-      if (aux->vertice == w)
-        return;
+    /*
+     * Se o grafo já tem a aresta
+     * [ v - w ], a função não faz nada.
+     */
+    if (aux->vertice == w)
+      return;
 
-      ant = aux; /* guarda o anterior */
-      aux = aux->proximo; /* anda para frente */
-    }
+    ant = aux; /* guarda o anterior */
+    aux = aux->proximo; /* anda para frente */
+  }
 
   novo = (TNo *) calloc (1, sizeof (TNo));
   novo->vertice = w;
@@ -128,17 +134,17 @@ int indeg (TGrafo *G, int w)
   int grau_de_entrada = 0;
 
   for (i = 0; i < controle; i++)
+  {
+    aux = G->adj[i];
+    while (aux && aux->vertice)
     {
-      aux = G->adj[i];
-      while (aux && aux->vertice)
-        {
-          if (aux->vertice == w)
-            {
-              grau_de_entrada++;
-            }
-          aux = aux->proximo; /* anda para frente */
-        }
+      if (aux->vertice == w)
+      {
+        grau_de_entrada++;
+      }
+      aux = aux->proximo; /* anda para frente */
     }
+  }
   return grau_de_entrada;
 }
 
@@ -149,23 +155,23 @@ int isPath (TGrafo *G, const int *seq, int k)
 {
   int i;
   for (i = 0; i < k; i++)
-    {
-      int v = seq[i];
-      int w = seq[i + 1];
+  {
+    int v = seq[i];
+    int w = seq[i + 1];
 
-      /* a partir da entrada [ v ] verifica os seus adjacentes */
-      TNo *aux = G->adj[v];
-      while (aux && aux->vertice != w)
-        aux = aux->proximo;
+    /* a partir da entrada [ v ] verifica os seus adjacentes */
+    TNo *aux = G->adj[v];
+    while (aux && aux->vertice != w)
+      aux = aux->proximo;
 
-      /*
-       * se  [ w ] não está na lista [ v ], não existe aresta e
-       * consequentemente, não existe caminho
-       */
-      if (aux == NULL)
-        return 0;
+    /*
+     * se  [ w ] não está na lista [ v ], não existe aresta e
+     * consequentemente, não existe caminho
+     */
+    if (aux == NULL)
+      return 0;
 
-    }
+  }
 
   /* É caminho */
   return 1;
@@ -181,19 +187,83 @@ void show (TGrafo *G)
   int i;
   printf ("Vértices [ %d ] e Arestas [ %d ]\n\n", G->V, G->A);
   for (i = 0; i < G->V; i++)
+  {
+    TNo *aux;
+    printf ("Vértice [ %d ] -> ", i);
+
+    /* para cada entrada da lista pega o início da lista */
+    aux = G->adj[i];
+
+    /* enquanto não chegar ao final da lista */
+    while (aux != NULL)
     {
-      TNo *aux;
-      printf ("Vértice [ %d ] -> ", i);
-
-      /* para cada entrada da lista pega o início da lista */
-      aux = G->adj[i];
-
-      /* enquanto não chegar ao final da lista */
-      while (aux != NULL)
-        {
-          printf ("%d ", aux->vertice);
-          aux = aux->proximo;
-        }
-      printf ("\n");
+      printf ("%d ", aux->vertice);
+      aux = aux->proximo;
     }
+    printf ("\n");
+  }
+}
+
+void busca (TGrafo *G)
+{
+  int qtdVertices = G->V;
+
+  /* Primeiro índice será ignorado */
+  int visitado[qtdVertices];
+
+  int i;
+  for (i = 0; i < qtdVertices; i++)
+  {
+    visitado[i] = 0;
+  }
+
+  for (i = 0; i < qtdVertices; i++)
+  {
+    TNo *aux = NULL;
+    aux = G->adj[i];
+    int debug = aux->vertice;
+    busca_profundidade (G, aux, visitado);
+  }
+  visitado;
+
+}
+void busca_profundidade (TGrafo *G, TNo *V, int visitado[])
+{
+  int vertice = V->vertice;
+  int visitadoAddress = vertice - 1;
+  int isVisitado = visitado[visitadoAddress];
+  if (isVisitado)
+    return;
+
+  /* Marca V como visitado */
+  visitado[visitadoAddress] = 1;
+
+  /* Imprime V */
+  printf (" %d", vertice);
+
+  TNo *aux = NULL;
+  aux = V->proximo;
+  while (aux != NULL)
+  {
+    busca_profundidade (G, aux, visitado);
+    aux = aux->proximo;
+  }
+}
+
+void freeGraph (TGrafo *G)
+{
+
+  int i;
+  for (i = 0; i < G->V; i++)
+  {
+    TNo *aux;
+    TNo *proximo;
+    aux = G->adj[i];
+    do
+    {
+      proximo = aux->proximo;
+      free (aux);
+    }
+    while (proximo != NULL);
+  }
 }
