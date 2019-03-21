57ab9480633a14543a652440f8fab0443080e346
Leonardo Fuso Nuzzo <leonardofusonuzzo@gmail.com>
2019-03-20 23:36:05 -0300

    dev - Ordenação ainda não está funcionando.

--- ordenacao_topologica.c
+++ ordenacao_topologica.c
@@ -4,8 +4,13 @@
 
 #include "ordenacao_topologica.h"
 
+struct ComparisonNode {
+    unsigned int vertice;
+    unsigned int grau_de_entrada;
+};
+
 TGrafo *organiza_grafo (TEntrada *entrada);
-int *ordena_tarefas (TGrafo *);
+struct ComparisonNode **ordena_tarefas (TGrafo *);
 int comparator (const void *, const void *);
 
 void ordenar (char *filename)
@@ -44,44 +49,56 @@ void ordenar (char *filename)
   grafo = organiza_grafo (entrada);
   show (grafo);
 
-  int * tarefasOrdenadas = ordena_tarefas (grafo);
+  printf ("%d", grau_de_entrada (grafo, 12));
+
+  struct ComparisonNode **tarefasOrdenadas = ordena_tarefas (grafo);
 
   for (i = 0; i < entrada->qtd_tarefas; ++i)
   {
-    printf ("Tarefa [ %d ]\n", tarefasOrdenadas[i] + 1);
+    printf ("Tarefa [ %d ]\n", tarefasOrdenadas[i]->vertice + 1);
   }
 }
 
 int
 comparator (const void *c1, const void *c2)
 {
-  int o1 = *(int*) c1;
-  int o2 = *(int*) c2;
+  struct ComparisonNode *o1 = *(struct ComparisonNode **) c1;
+  struct ComparisonNode *o2 = *(struct ComparisonNode **) c2;
 
-  if (o1 > o2)
+  if (o1->grau_de_entrada > o2->grau_de_entrada)
     return 1;
-  else if (o1 < o2)
+  else if (o1->grau_de_entrada < o2->grau_de_entrada)
+    return -1;
+  else if ((o1->grau_de_entrada = o2->grau_de_entrada) && (o1->vertice > o2->vertice))
+    return 1;
+  else if ((o1->grau_de_entrada = o2->grau_de_entrada) && (o1->vertice < o2->vertice))
     return -1;
   else
     return 0;
 }
 
-int *ordena_tarefas (TGrafo *g)
+struct ComparisonNode **
+ordena_tarefas (TGrafo *g)
 {
-
   unsigned int qtdTarefas = g->qtd_vertices;
 
-  int *tarefas = NULL;
-  tarefas = (int *) calloc (qtdTarefas, sizeof (int));
+  struct ComparisonNode *aux = NULL;
+  struct ComparisonNode **nodes = NULL;
+  nodes = (struct ComparisonNode **) calloc (qtdTarefas, sizeof (struct ComparisonNode *));
 
   unsigned int grau_aux = 0;
   unsigned int i;
   for (i = 0; i < qtdTarefas; ++i)
-    tarefas[i] = grau_de_entrada (g, i);
+  {
+    aux = (struct ComparisonNode *) calloc (1, sizeof (struct ComparisonNode));
+    aux->vertice = i;
+    aux->grau_de_entrada = grau_de_entrada (g, i);
+    nodes[i] = aux;
+  }
 
-  qsort (tarefas, qtdTarefas, sizeof (int), comparator);
+  qsort (nodes, qtdTarefas, sizeof (struct ComparisonNode *), comparator);
 
-  return tarefas;
+  return nodes;
 }
 
 TGrafo *organiza_grafo (TEntrada *entrada)
